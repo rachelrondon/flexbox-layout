8568a109511e042df8010e479e5894f6671a7e10
Rachel <rachel.sun.rondon@gmail.com>
2025-07-12 17:51:04 -0400

    fix: update classname

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 import './Card.css';
 
-const Card = ({ className }) => {
+const Card = ({className}) => {
   return (
     <div className={className}>
 
--- src/Layout.js
+++ src/Layout.js
@@ -4,32 +4,18 @@ import Card from './Card';
 const Layout = () => {
   return (
     <div className="card-container">
-      <div className="card-top">
-        <Card 
-          className="card card-one" 
-          />
-        <Card 
-          className="card card-two" 
-          />
+      <div className="card-container-top">
+        <Card className="card card-one" />
+        <Card className="card card-two" />
       </div>
-      <div className="card-middle">
-        <Card 
-          className="card card-three" 
-        />
-        <Card 
-          className="card card-four" 
-        />
-        <Card 
-          className="card card-five" 
-        />
+      <div className="card-container-middle">
+        <Card className="card card-three" />
+        <Card className="card card-four" />
+        <Card className="card card-five" />
       </div>
-      <div className="card-bottom">
-        <Card 
-          className="card card-six" 
-        />
-        <Card 
-          className="card card-seven" 
-        />
+      <div className="card-container-bottom">
+        <Card className="card card-six" />
+        <Card className="card card-seven" />
       </div>
     </div>
   )
